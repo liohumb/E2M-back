3617d8306f70811fbb8ec32628b1822a121d090b
liohumb <helloliohumb@gmail.com>
2023-03-16 15:55:01 +0100

    mise ne place des routes pour les socials

--- routes/socials.js
+++ routes/socials.js
@@ -3,13 +3,19 @@ const Social = require( '../models/Social' )
 
 /* CREATE */
 router.post( '/', async ( req, res ) => {
-    const newSocial = new Social( req.body )
+    const { user, ...socialData } = req.body
+
+    const newSocial = new Social( {
+        ...socialData,
+        user
+    } )
 
     try {
         const savedSocial = await newSocial.save()
-        res.status( 200 ).json( savedSocial )
+        res.status( 201 ).json( savedSocial )
     } catch (e) {
-        res.status( 500 ).json( { e: e.message } )
+        console.error( e )
+        res.status( 500 ).json( { message: e.message } )
     }
 } )
 
@@ -26,35 +32,34 @@ router.get( '/:id', async ( req, res ) => {
 /* GET ALL */
 router.get( '/', async ( req, res ) => {
     try {
-        const socials = await Social.find()
-        res.status( 200 ).json( socials )
-    } catch (e) {
-        res.status( 500 ).json( { e: e.message } )
+        const user = req.query.user
+        const socials = await Social.find( { user: user } )
+        res.json( socials )
+    } catch (err) {
+        console.error( e )
+        res.status( 500 ).json( { message: e.message } )
     }
 } )
 
 /* UPDATE */
 router.put( '/:id', async ( req, res ) => {
+    const { id } = req.params
+    const updatedSocial = req.body
+
     try {
-        const social = await Social.findById( req.params.id )
+        const social = await Social.findByIdAndUpdate( id, updatedSocial, {
+            new: true,
+            useFindAndModify: false
+        } )
 
-        if (social.user.toString() === req.body.user._id) {
-            try {
-                const updatedSocial = await Social.findByIdAndUpdate(
-                    req.params.id,
-                    { $set: req.body },
-                    { new: true }
-                )
-
-                res.status( 200 ).json( updatedSocial )
-            } catch (e) {
-                res.status( 500 ).json( { e: e.message } )
-            }
+        if (!social) {
+            res.status( 404 ).json( { message: 'Réseaux social introuvable' } )
         } else {
-            res.status( 401 ).json( { message: 'Vous ne pouvez pas modifier un élément Social qui n\'est pas lié à votre compte' } )
+            res.json( social )
         }
     } catch (e) {
-        res.status( 500 ).json( { e: e.message } )
+        console.error( e )
+        res.status( 500 ).json( { message: e.message } )
     }
 } )
 
@@ -63,7 +68,12 @@ router.delete( '/:id', async ( req, res ) => {
     try {
         const social = await Social.findById( req.params.id )
 
-        if (social.user.toString() === req.body.user._id) {
+        if (!req.user || !req.user._id) {
+            res.status( 401 ).json( { message: 'Authentification requise' } )
+            return
+        }
+
+        if (social.user.toString() === req.user._id.toString()) {
             try {
                 await social.delete()
                 res.status( 200 ).json( { message: 'L\'élément Social a bien été supprimé' } )
